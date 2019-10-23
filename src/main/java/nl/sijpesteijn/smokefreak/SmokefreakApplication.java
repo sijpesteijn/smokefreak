dbd81d3cfe1f5cc2bc226f1598295156574058e5
Gijs sijpesteijn <gijsbert.sijpesteijn@gmail.com>
2019-10-23 22:57:56 +0200

    response logging

--- src/main/java/nl/sijpesteijn/smokefreak/SmokefreakApplication.java
+++ src/main/java/nl/sijpesteijn/smokefreak/SmokefreakApplication.java
@@ -1,12 +1,17 @@
 package nl.sijpesteijn.smokefreak;
 
+import ch.qos.logback.access.servlet.TeeFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @SpringBootApplication
 public class SmokefreakApplication {
@@ -20,4 +25,16 @@ public class SmokefreakApplication {
 		SpringApplication.run(SmokefreakApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean requestResponseFilter() {
+
+		TeeFilter filter = new TeeFilter();
+		final FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+		filterRegBean.setFilter(filter);
+		Collection<String> urls = Collections.singleton("/api/");
+		filterRegBean.setUrlPatterns(urls);
+		filterRegBean.setName("Request Response Filter");
+		filterRegBean.setAsyncSupported(Boolean.TRUE);
+		return filterRegBean;
+	}
 }
--- src/main/java/nl/sijpesteijn/smokefreak/endpoints/SettingsEndpoint.java
+++ src/main/java/nl/sijpesteijn/smokefreak/endpoints/SettingsEndpoint.java
@@ -15,40 +15,39 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/api/settings")
 public class SettingsEndpoint {
-    private final Mono<SecurityContext> context = ReactiveSecurityContextHolder.getContext();
+  private final Mono<SecurityContext> context = ReactiveSecurityContextHolder.getContext();
 
-    private SettingsRepository settingsRepository;
+  private SettingsRepository settingsRepository;
 
-    public SettingsEndpoint(SettingsRepository settingsRepository) {
+  public SettingsEndpoint(SettingsRepository settingsRepository) {
+    this.settingsRepository = settingsRepository;
+  }
 
-        this.settingsRepository = settingsRepository;
-    }
+  @GetMapping()
+  private Mono<ResponseEntity<Settings>> getSettings() {
+    return extractUserSeqIdFromJwtToken(context).flatMap(jwt ->
+    {
+      String username = (String) jwt.getClaims().get("preferred_username");
+      return settingsRepository
+          .findById(username)
+          .map(settings -> ResponseEntity.ok(settings))
+          .defaultIfEmpty(ResponseEntity.notFound().build());
+    });
+  }
 
-    @GetMapping()
-    private Mono<ResponseEntity<Settings>> getSettings() {
-        return extractUserSeqIdFromJwtToken(context).flatMap(jwt ->
-        {
-            String username = (String) jwt.getClaims().get("preferred_username");
-            return settingsRepository
-                    .findById(username)
-                    .map(settings -> ResponseEntity.ok(settings))
-                    .defaultIfEmpty(ResponseEntity.notFound().build());
-        });
-    }
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  private Mono<ResponseEntity<Settings>> save(@Valid @RequestBody Settings settings) {
+    return extractUserSeqIdFromJwtToken(context).flatMap(jwt ->
+    {
+      settings.setUsername((String) jwt.getClaims().get("preferred_username"));
+      return settingsRepository.save(settings).map(saved -> ResponseEntity.ok(saved));
+    });
+  }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    private Mono<ResponseEntity<Settings>> save(@Valid @RequestBody Settings settings) {
-        return extractUserSeqIdFromJwtToken(context).flatMap(jwt ->
-        {
-            settings.setUsername((String) jwt.getClaims().get("preferred_username"));
-            return settingsRepository.save(settings).map(saved -> ResponseEntity.ok(saved));
-        });
-    }
-
-    private Mono<Jwt> extractUserSeqIdFromJwtToken(Mono<SecurityContext> context) {
-        return context.filter(c -> Objects.nonNull(c.getAuthentication()))
-                .map(s -> s.getAuthentication().getPrincipal())
-                .cast(Jwt.class);
-    }
+  private Mono<Jwt> extractUserSeqIdFromJwtToken(Mono<SecurityContext> context) {
+    return context.filter(c -> Objects.nonNull(c.getAuthentication()))
+        .map(s -> s.getAuthentication().getPrincipal())
+        .cast(Jwt.class);
+  }
 
 }
