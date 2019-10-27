40b695ae188e7be968b0c25743d309bb4d0d4b72
gijs.sijpesteijn <gijs@jpoint.nl>
2019-10-27 20:07:27 +0100

    Nginx problems

--- src/main/java/nl/sijpesteijn/smokefreak/SmokefreakApplication.java
+++ src/main/java/nl/sijpesteijn/smokefreak/SmokefreakApplication.java
@@ -2,13 +2,14 @@ package nl.sijpesteijn.smokefreak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SmokefreakApplication {
 
 	@Bean
