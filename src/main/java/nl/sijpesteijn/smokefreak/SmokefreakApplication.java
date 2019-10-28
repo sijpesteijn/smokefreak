7d92c7800058f16cf7aaee40088b5b4c05040931
Gijs sijpesteijn <gijsbert.sijpesteijn@gmail.com>
2019-10-28 08:21:36 +0100

    Removed static resources

--- src/main/java/nl/sijpesteijn/smokefreak/SmokefreakApplication.java
+++ src/main/java/nl/sijpesteijn/smokefreak/SmokefreakApplication.java
@@ -2,20 +2,9 @@ package nl.sijpesteijn.smokefreak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class SmokefreakApplication {
-
-	@Bean
-	RouterFunction<ServerResponse> staticResourceRouter(){
-		return RouterFunctions.resources("/**", new ClassPathResource("smokefreak-app/public/"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmokefreakApplication.class, args);
